3ed69bf4b76fbb64b2c70d2bba59ef13e43daccb
Zongmin Lei <leizongmin@gmail.com>
2017-03-13 15:30:52 +0800

    v2.0.2 fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-import * as express from 'express';
+import { Router, RequestHandler, ErrorRequestHandler } from 'express';
 
 /**
  * 包装 router，使其所有注册的 handler 均可支持 async function
  */
-export function wrapRouter(router: express.Router): express.Router;
+export function wrapRouter(router: Router): Router;
 
 /**
  * 包装 handler，使其支持 async function
  */
-export function wrapHandler(handler: express.Handler, withErrorParam?: boolean): express.Router;
+export function wrapHandler(handler: RequestHandler | ErrorRequestHandler, withErrorParam?: boolean): Router;
